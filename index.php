51008ffcc5cdc106488c62dfac5f804cf364a041
Sunnyface <kiko@sunnyface.com>
2016-06-07 16:15:03 +0200

    Show Rafa

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-require_once("lib/base.inc.php");
+require_once 'lib/base.inc.php';
 $s = new Session(false);
 $title = 'Public page';
-include('parts/header.php');
+include 'parts/header.php';
 ?>
 
 
 <div class="jumbotron">
   <h1 class="display-3">Welcome to <?php echo $s->web->name;?></h1>
-  <p class="lead">This is a very simple PHP framework made for fast PHP proccessing and development, ADO + Basic Helper Classes to get you going on a development fast.</p>
+  <p class="lead">This is a very simple PHP. framework made for fast PHP proccessing and development, ADO + Basic Helper Classes to get you going on a development fast.</p>
   <hr class="m-y-md">
   <p>Use it as you wish, and if you want to colaborate you are very welcome to push your commits to the master source.</p>
   <p class="lead">
@@ -17,6 +17,5 @@ include('parts/header.php');
 </div>
 
 
-
 <?php
-include('parts/footer.php');
+include 'parts/footer.php';
